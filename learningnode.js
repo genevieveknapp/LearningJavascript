8eeaab6e3475b8bd0c9e9a43a211a3f653a03147
genevieve <genevieve.knapp@gmail.com>
2020-03-05 19:30:35 -0600

    Basic Server

--- learningnode.js
+++ learningnode.js
@@ -1,6 +1,14 @@
-let strawberryCount = 20
-if(strawberryCount > 10) {
-    console.log("You have enough berries.")
-} else {
-    console.log("You do not have enough berries, sorry")
-}
+let http = require("http")
+
+let ourApp = http.createServer(function(req, res){
+    if (req.url == "/") {
+        res.end("Hello, and welcome to bird's homepage.")
+    }
+
+    if (req.url == "/about") {
+        res.end("Hello, thanks for trying to learn about bird.")
+    }
+
+    res.end("We cannot find the page for which you are searching.")
+})
+ourApp.listen(3000)
