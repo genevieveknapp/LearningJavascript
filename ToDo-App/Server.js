8f4b9b74d2057bb839f6d6124278bf125e71228a
genevieve <genevieve.knapp@gmail.com>
2020-03-13 20:17:53 -0500

    connect to mongodb

--- ToDo-App/Server.js
+++ ToDo-App/Server.js
@@ -1,7 +1,15 @@
 let express = require('express')
+let mongodb = require('mongodb')
 let path = require('path');
 
 let app = express()
+let db
+
+let connectionString = "mongodb+srv://todoAppUser:Bird-Man88@cluster001-rugsw.gcp.mongodb.net/ToDoApp?retryWrites=true&w=majority"
+mongodb.connect(connectionString, {userNewUrlParser: true}, function(err, client) {
+    db = client.db()
+    app.listen(3000)
+})
 
 app.use(express.urlencoded({extended: false}))
 
@@ -10,7 +18,7 @@ app.get('/', function(req, res) {
 })
 
 app.post(`/create-item`, function(req, res) {
-    console.log(req.body.item)
-    res.send("Thanks for submitting the form.")
+    db.collection('Items').insertOne({text: req.body.item}, function(){
+        res.send("Thanks for submitting the form.")
+    })
 })
-app.listen(3000)
