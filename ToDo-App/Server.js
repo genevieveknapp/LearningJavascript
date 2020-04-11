a32a172ac47883c1a6c2dfb2f52faac0c36f1e09
genevieve <genevieve.knapp@gmail.com>
2020-04-11 18:48:08 -0500

    Add proc file

--- ToDo-App/Server.js
+++ ToDo-App/Server.js
@@ -5,13 +5,17 @@ let sanitizeHTML = require('sanitize-html')
 
 let app = express()
 let db
+let port = process.env.PORT
+if (port == null || port == ""){
+  port = 3000
+}
 
 app.use(express.static("public"))
 
 let connectionString = "mongodb+srv://todoAppUser:Bird-Man88@cluster001-rugsw.gcp.mongodb.net/ToDoApp?retryWrites=true&w=majority"
 mongodb.connect(connectionString, {userNewUrlParser: true}, function(err, client) {
     db = client.db()
-    app.listen(3000)
+    app.listen(port)
 })
 
 app.use(express.json())
